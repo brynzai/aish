51d4341c39bd6c4750b4975e8d7abde592a263ea
JohnnyB <jboero@users.noreply.github.com>
2024-02-01 12:53:34 +0000

    Updated default model.
    I was hasty and brought great shame to my commit.

--- plugins/openai.cpp
+++ plugins/openai.cpp
@@ -39,7 +39,7 @@ int shellgpt(const string &cmd)
 			Json::Value history;
 			history["role"] = "system";
 			
-			thread["model"] = getenvsafe("OPENAI_MODEL", "gpt-3.5-turbo");;
+			thread["model"] = getenvsafe("OPENAI_MODEL", "gpt-3.5-turbo");
 			thread["temperature"] = temperature;
 			thread["messages"] = Json::arrayValue;
 			thread["messages"].append(history);
@@ -111,7 +111,7 @@ int chatgpt(const string &cmd)
 			Json::Value history;
 			history["role"] = "system";
 
-			thread["model"] = getenvsafe("OPENAI_MODEL", "https://api.openai.com/v1/chat/completions");
+			thread["model"] = getenvsafe("OPENAI_MODEL", "gpt-3.5-turbo");
 			thread["temperature"] = temperature;
 			thread["messages"] = Json::arrayValue;
 			thread["messages"].append(history);
